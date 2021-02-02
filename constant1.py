eb87de510a542bf8e21767471fc72695b2528af2
yingfengli <lyfwww@gmail.com>
2021-02-02 15:48:08 +0800

    Add files via upload

---
+++ constant1.py
@@ -0,0 +1,8 @@
+#DATABASE_NAME = "devops"
+#TABLE_NAME = "host_metrics"
+#HT_TTL_HOURS = 24
+#CT_TTL_DAYS = 7
+DATABASE_NAME = "stock"
+TABLE_NAME = "stock_metrics"
+HT_TTL_HOURS = 24
+CT_TTL_DAYS = 7
